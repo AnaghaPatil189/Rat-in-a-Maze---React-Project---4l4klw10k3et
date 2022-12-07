8a786857e88b5c41bc9abad30e62940a5de99c63
AnaghaPatil189 <109735323+AnaghaPatil189@users.noreply.github.com>
2022-12-08 00:41:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Problem from "./components/Problem";
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className="Heading"> RAT IN A MAZE </h3>
+      <Problem />
+    </div>
+  );
+}
+
+export default App;
